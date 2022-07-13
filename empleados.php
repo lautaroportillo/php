c80d51565f8af90bb1603d369767e3b4a627acf7
lautaroportillo <portillolautaro 1010@gmail.com>
2022-07-13 19:08:33 -0300

    Cambios

--- empleados.php
+++ empleados.php
@@ -4,21 +4,21 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function calcularNeto($bruto){
+
+//definicion de empleados
+$aEmpleados = array();
+$aEmpleados[] = array("dni" => 33300123, "nombre" => "David Garcia", "bruto" => 85000.30);
+$aEmpleados[] = array("dni" => 40874456, "nombre" => "Ana Del Valle", "bruto" => 90000);
+$aEmpleados[] = array("dni" => 67567565, "nombre" => "Andres Perez", "bruto" => 100000);
+$aEmpleados[] = array("dni" => 75744545, "nombre" => "Victoria Luz", "bruto" => 70000);
+
+
+function calcularNeto($bruto)
+{
     return $bruto - ($bruto * 0.17);
 }
 
-echo "el sueldo neto es $" . calcularNeto(85000.30);
 
-$importe= 85000.30 & 9000 & 100000 & 70000;
-echo "$" .number_format($importe, 2 , "." , ",");
-
-//definicion de empleados
-$aEmpleados = array ();
-$aEmpleados[] = array("dni" => 33300123, "nombre" => "David Garcia", "Bruto" => 85000.30);
-$aEmpleados[] = array("dni" => 40874456, "nombre" => "Ana Del Valle", "Bruto" => 90000);
-$aEmpleados[] = array("dni" => 67567565, "nombre" => "Andres Perez", "Bruto" => 100000);
-$aEmpleados[]= array("dni" => 75744545, "nombre" => "Victoria Luz", "Bruto" => 70000);
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -35,8 +35,8 @@ $aEmpleados[]= array("dni" => 75744545, "nombre" => "Victoria Luz", "Bruto" => 7
 <body>
     <main class="container">
         <dvi class="row">
-            <div class="col-12 text-center">
-                <h1 class="py-5">Listado de empleados</h1>
+            <div class="col-12 text-center py-5">
+                <h1>Listado de empleados</h1>
             </div>
             <div class="row">
                 <div class="col-12">
@@ -50,19 +50,22 @@ $aEmpleados[]= array("dni" => 75744545, "nombre" => "Victoria Luz", "Bruto" => 7
                         <tbody>
 
 
-                        <?php foreach ($aEmpleados as $aEmpleado ) {?>
-                        <tr>
-                        <td><?php echo $aEmpleados [$i] ["dni"]; ?></td>
-                        <td><?php echo $aEmpleados [$i] ["nombre"]; ?></td>
-                        <td><?php echo $aEmpleados [$i] ["neto"]; ?></td>
-                    </tr>
-                    <?php } ?>
+                            <?php foreach ($aEmpleados as $empleado) {
+                                print_r($empleado);
+                                exit;
+                            ?>
+                                <tr>
+                                    <td><?php echo $empleado["dni"]; ?></td>
+                                    <td><?php echo mb_strtoupper($empleado  ["nombre"]); ?></td>
+                                    <td>$<?php echo number_format ($empleado  ( CalcularNeto( $empleado ["bruto"])), 2 , ".", ","); ?></td>
+                                </tr>
+                            <?php }?>
                         </tbody>
                     </table>
                 </div>
                 <div class="row">
                     <div class="col-12">
-                        <p>cantidad de empleados: <?php echo $sumatoriaprecios ?></p>
+                        <p>cantidad de empleados activos: <?php echo count($aEmpleados);?></p>
 
                     </div>
 
