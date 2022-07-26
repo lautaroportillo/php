1db097af082d791366aca372e4f29107f16b84a9
lautaroportillo <portillolautaro 1010@gmail.com>
2022-07-25 21:00:06 -0300

    Cambios

--- listado_clientes.php
+++ listado_clientes.php
@@ -7,32 +7,50 @@ error_reporting(E_ALL);
 
 session_start();
 
-if(isset($_SESSION["listadoClientes"])){
+if (isset($_SESSION["listadoClientes"])) {
     //Si existe la variable de session listadoClientes asigno su contenido a aClientes
     $aClientes = $_SESSION["listadoClientes"];
-}else{
+} else {
     $aClientes = array();
 }
 
-if ($_POST){
+if ($_POST) {
+    //si hace click en enviar
+    if (isset($_POST["btnEnviar"])) {
+        //Asignamos en variables los datos que vienen del formulario
+        $nombre = $_POST["txtNombre"];
+        $dni = $_POST["txtDni"];
+        $telefono = $_POST["txtTelefono"];
+        $edad = $_POST["txtEdad"];
 
-    //Asignamos en variables los datos que vienen del formulario
-    $nombre = $_POST["txtNombre"];
-    $dni = $_POST["txtDni"];
-    $telefono = $_POST["txtTelefono"];
-    $edad = $_POST["txtEdad"];
+        //creamos un array que contendra el listado de clientes
 
-//creamos un array que contendra el listado de clientes
-
-$aClientes[] = array("nombre" => $nombre,
-                     "dni" => $dni,
-                      "telefono" => $telefono,
-                       "edad" => $edad
-);
-//actualiza el contenido de variable de session
-$_SESSION["listadoClientes"] = $aClientes;
-
+        $aClientes[] = array(
+            "nombre" => $nombre,
+            "dni" => $dni,
+            "telefono" => $telefono,
+            "edad" => $edad
+        );
+        //actualiza el contenido de variable de session
+        $_SESSION["listadoClientes"] = $aClientes;
+    }
+    //si hace click en eliminar
+    if (isset($_POST["btnEliminar"])) {
+        session_destroy();
+        $aClientes = array();
+    }
 }
+
+if (isset($_GET["pos"])) {
+    //Recupero el dato que viene desde la query string via get
+    $pos = $_GET["pos"];
+    //Elimina la posicion del array indicada
+    unset($aClientes[$pos]);
+    //Actualizo l avariable de session con el array actualizado
+    $_SESSION["listadoClientes"] = $aClientes;
+    header("Location: listado_clientes.php");
+}
+
 
 
 ?>
@@ -45,6 +63,7 @@ $_SESSION["listadoClientes"] = $aClientes;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <title>Listado de clientes</title>
 </head>
 
@@ -56,7 +75,7 @@ $_SESSION["listadoClientes"] = $aClientes;
             </div>
             <div class="row">
                 <div class="col-4">
-                    <form method="POST">
+                    <form method="POST" action="">
                         <div class="my-3">
                             <table for="">Nombre:</table>
                             <input type="text" id="txtNombre" name="txtNombre" class="form-control">
@@ -87,17 +106,19 @@ $_SESSION["listadoClientes"] = $aClientes;
                                 <th>DNI:</th>
                                 <th>Telefono:</th>
                                 <th>Edad:</th>
+                                <th>Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
-                        <?php foreach ($aClientes as $cliente): ?>
-                            <tr>
-                                <td><?php echo $cliente["nombre"]; ?></td>
-                                <td><?php echo $cliente["dni"]; ?></td>
-                                <td><?php echo $cliente["telefono"]; ?></td>
-                                <td><?php echo $cliente["edad"]; ?></td>
-                            </tr>
-                        <?php endforeach; ?>
+                            <?php foreach ($aClientes as $pos => $cliente) : ?>
+                                <tr>
+                                    <td><?php echo $cliente["nombre"]; ?></td>
+                                    <td><?php echo $cliente["dni"]; ?></td>
+                                    <td><?php echo $cliente["telefono"]; ?></td>
+                                    <td><?php echo $cliente["edad"]; ?></td>
+                                    <td><a href="listado_clientes.php?pos=<?php echo $pos; ?>"><i class="bi bi-trash"></i></a></td>
+                                </tr>
+                            <?php endforeach; ?>
                         </tbody>
                     </table>
                 </div>
---
+++ print_f.php
@@ -0,0 +1,36 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//Definición
+function print_f($variable){
+	if(is_array($variable)){
+        $archivo = fopen('datos.txt', 'a+');
+
+
+
+
+        //Si es un array, lo recorro y guardo el contenido en el archivo “datos.txt”
+        foreach ($variable as $item);{
+            fwrite($archivo, $item . "n");
+
+        }
+        fclose($archivo);
+	} else {
+        //Entonces es string, guardo el contenido en el archivo “datos.txt”
+        file_put_contents("datos.txt", $variable);
+}
+}
+//Uso
+$aNotas = array(8,5,7,9,10);
+$msg = "Este es un mensaje";
+print_f($aNotas);
+print_f($msg);
+
+
+?>
